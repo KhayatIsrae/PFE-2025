6debdc79e18d4d5e0a2e49b5c2d6a8121e115740
KhayatIsrae <israe.khayat@usmba.ac.ma>
2025-03-01 18:06:26 +0000

    login method changed

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,19 +1,71 @@
+const fs = require('fs');
+const { disconnect } = require('process');
+
+const loadDatabase = () => {
+    return fs.promises.readFile("public/BD/bd.json")
+        .then(buffer => buffer.toString())
+        .then(string => JSON.parse(string))
+        .then(data => data.etudiants)
+        .catch(e => console.log(e))
+}
+
+const verifierUtilisateur = (email, password) => {
+    return loadDatabase().then(etudiants => {
+        const utilisateur = etudiants.find(user => user.email === email);
+        if (!utilisateur) {
+            return -1;
+        }
+        if (utilisateur.mdp !== password) {
+            return 0;
+        }
+        return 1;
+    })
+};
+const username = async (email) => {
+    return loadDatabase()
+        .then(etudiants => {
+            let utilisateur = etudiants.find(user => user.email === email);
+            return utilisateur.username
+        })
+}
 
 module.exports = {
     get: (req, res) => {
-        let WALLET_CONNECTED
-        if (!req.session.WALLET_CONNECTED || req.session.WALLET_CONNECTED == "") {
-            WALLET_CONNECTED = ""
+        let utilisateur
+        if (!req.session.utilisateur || req.session.utilsateur == "") {
+            utilisateur = ""
             req.session.destroy()
         } else {
-            WALLET_CONNECTED = req.session.WALLET_CONNECTED
+            utilisateur = req.session.utilisateur
         }
-        
-        res.render("index.ejs", { WALLET_CONNECTED })
+        // console.log("user",utilisateur)
+        res.render("index.ejs", { utilisateur })
     }
     ,
     post: (req, res) => {
-        req.session.WALLET_CONNECTED=req.body.WALLET_CONNECTED
-        res.redirect("/")
+        let mail = req.body.mail
+        let mdp = req.body.mdp
+        let disconnect = req.body.disconnect
+        if (disconnect) {
+            req.session.utilisateur = ""
+            return res.redirect("/")
+        }
+        else {
+            verifierUtilisateur(mail, mdp)
+                .then(result => {
+                    if (result == -1) {
+                        return res.status(404).json({ msg: "Utilisateur non trouvé" });
+                    } else if (result) {
+                        username(mail)
+                            .then(username => {
+                                req.session.utilisateur = username
+                                return res.redirect("/")
+                            })
+                    } else {
+                        return res.status(401).json({ msg: "Mot de passe incorrect" });
+                    }
+                })
+        }
+
     }
 }
--- public/js/script.js
+++ public/js/script.js
@@ -8,48 +8,38 @@ let popUp = document.getElementById("popUp");
 let closeButton = popUp.querySelector(".btnAnnuller");
 let message = document.createElement("p");
 let link = document.getElementById("popUpA");
-const connectMetamask = async () => {
-    if (typeof window.ethereum === 'undefined') {
-        popUp.setAttribute("class", "popUpErr")
-        message.textContent = "MetaMask n'est pas installé.";
-        link.setAttribute("href", "https://metamask.io/download/");
-        link.setAttribute("target", "_blank")
-        link.textContent = "Installer MetaMask";
-        link.removeAttribute("class");
-    } else {
-        try {
-            const provider = new ethers.providers.Web3Provider(window.ethereum)
-            await provider.send("eth_requestAccounts", [])
-            const signer = provider.getSigner()
-            WALLET_CONNECTED = await signer.getAddress()
-            fetch(url, {
-                method: "POST",
-                body: JSON.stringify({ WALLET_CONNECTED }),
-                headers: { "Content-Type": "application/json" }
-            })
-                .then(response => window.location.reload())
-                .catch(e => console.log(e))
-        } catch (erreur) {
-            console.error("Error connecting to MetaMask", error);
-        }
-    }
-    return { WALLET_CONNECTED };
+
+const logIn = (mail, mdp) => {
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify({ mail, mdp }),
+        headers: { "Content-Type": "application/json" }
+    })
+        .then(async (response) => {
+            if (response.status >= 200 && response.status < 300) {
+                return window.location.reload()
+            } else {
+                response = await response.json()
+                message.textContent = response.msg
+                popUp.setAttribute("class", "popUpErr");
+            }
+        })
+        .catch(e => console.log("erreur:", e))
 }
-const disconnectMetamask = () => {
+const disconnect = () => {
     fetch(url, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ WALLET_CONNECTED: "" })
+        body: JSON.stringify({ disconnect: true })
     })
         .then(response => window.location.reload())
         .catch(e => console.error(e))
 }
-
 if (isConnected) {
     let btnDeconnection = document.getElementById("btnDeconnection")
     btnDeconnection.setAttribute("class", "btnDeconnection")
     btnDeconnection.addEventListener("click", () => {
-        disconnectMetamask()
+        disconnect()
     })
     const loginBox = document.querySelector(".login-box ")
     loginBox.style.width = "500px"
@@ -84,7 +74,9 @@ if (isConnected) {
 } else {
     let connectButton = document.getElementById("connectButton")
     connectButton.addEventListener("click", () => {
-        connectMetamask()
+        let email = document.getElementById("emailInput").value
+        let password = document.getElementById("passwordInput").value
+        logIn(email, password)
     })
 }
 popUp.appendChild(message);
